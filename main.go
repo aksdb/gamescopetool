ecb9fe96ee43d636c72f419d8fa9ae33a6b13c4a
Andreas Schneider <aksdb@gmx.de>
2022-06-12 20:04:19 +0200

    ✨ Make commandline parsing more robust

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
 	"os/exec"
+	"strings"
 	"sync"
 	"time"
 
@@ -18,7 +20,7 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	if err := clipboard.Init(); err != nil {
-		panic(err)
+		log.Fatalf("cannot initialize clipboard: %v", err)
 	}
 
 	if os.Args[1] == "-client" {
@@ -30,10 +32,9 @@ func main() {
 
 func runClient() {
 	clientSocket := os.Args[2]
-	fmt.Printf("client for %q\n", clientSocket)
 	client, err := ipc.StartClient(clientSocket, nil)
 	if err != nil {
-		panic(err)
+		log.Fatalf("cannot initialize client IPC: %v", err)
 	}
 	defer client.Close()
 
@@ -83,78 +84,86 @@ func runClient() {
 }
 
 func runServer() {
-	socketName := randomName()
-	fmt.Printf("server for %q\n", socketName)
-	server, err := ipc.StartServer(socketName, nil)
-	if err != nil {
-		panic(err)
-	}
-	defer server.Close()
-
-	var mtx sync.Mutex
-	var clipboardContent = clipboard.Read(clipboard.FmtText)
-	var watchCtx, cancelWatch = context.WithCancel(context.Background())
-	defer cancelWatch()
-
-	go func() {
-		for {
-			if server.StatusCode() == ipc.Connected {
-				break
-			}
-			time.Sleep(100 * time.Millisecond)
-		}
-
-		_ = server.Write(1, clipboardContent)
-
-		clipboardChanges := clipboard.Watch(watchCtx, clipboard.FmtText)
-		for newContent := range clipboardChanges {
-			mtx.Lock()
-			if !bytes.Equal(newContent, clipboardContent) {
-				_ = server.Write(1, newContent)
-				clipboardContent = newContent
-			}
-			mtx.Unlock()
-		}
-	}()
-
-	go func() {
-		for {
-			msg, err := server.Read()
-			if err != nil {
-				fmt.Printf("got error on server read: %v\n", err)
-				break
-			}
-			if msg.MsgType == 1 {
-				mtx.Lock()
-				clipboard.Write(clipboard.FmtText, msg.Data)
-				clipboardContent = msg.Data
-				mtx.Unlock()
-			}
-		}
-	}()
-
 	allArgs := os.Args[1:]
-	var gamescopeArgs []string
-	var gameArgs = allArgs
+	gamescopeArgCount := 0
+	gameCommandOffset := 0
 	for i := range allArgs {
+		if strings.HasPrefix(allArgs[i], "-") {
+			gamescopeArgCount++
+			gameCommandOffset++
+		}
 		if allArgs[i] == "--" {
-			gamescopeArgs = allArgs[:i]
-			gameArgs = allArgs[i+1:]
+			gameCommandOffset++
 			break
 		}
+	}
+	var gamescopeArgs = allArgs[:gamescopeArgCount]
+	var gameArgs = allArgs[gameCommandOffset:]
+
+	socketName := randomName()
+	if len(gameArgs) > 0 {
+		server, err := ipc.StartServer(socketName, nil)
+		if err != nil {
+			log.Fatalf("cannot initialize server IPC: %v", err)
+		}
+		defer server.Close()
+
+		var mtx sync.Mutex
+		var clipboardContent = clipboard.Read(clipboard.FmtText)
+		var watchCtx, cancelWatch = context.WithCancel(context.Background())
+		defer cancelWatch()
+
+		go func() {
+			for {
+				if server.StatusCode() == ipc.Connected {
+					break
+				}
+				time.Sleep(100 * time.Millisecond)
+			}
+
+			_ = server.Write(1, clipboardContent)
+
+			clipboardChanges := clipboard.Watch(watchCtx, clipboard.FmtText)
+			for newContent := range clipboardChanges {
+				mtx.Lock()
+				if !bytes.Equal(newContent, clipboardContent) {
+					_ = server.Write(1, newContent)
+					clipboardContent = newContent
+				}
+				mtx.Unlock()
+			}
+		}()
+
+		go func() {
+			for {
+				msg, err := server.Read()
+				if err != nil {
+					fmt.Printf("got error on server read: %v\n", err)
+					break
+				}
+				if msg.MsgType == 1 {
+					mtx.Lock()
+					clipboard.Write(clipboard.FmtText, msg.Data)
+					clipboardContent = msg.Data
+					mtx.Unlock()
+				}
+			}
+		}()
 	}
 
 	var args []string
 	args = append(args, gamescopeArgs...)
-	args = append(args, "--")
-	args = append(args, os.Args[0], "-client", socketName)
-	args = append(args, gameArgs...)
+	if len(gameArgs) > 0 {
+		args = append(args, "--")
+		args = append(args, os.Args[0], "-client", socketName)
+		args = append(args, gameArgs...)
+	}
 
 	clientCommand := exec.Command("gamescope", args...)
 	clientCommand.Stdout = os.Stdout
 	clientCommand.Stderr = os.Stderr
 	if err := clientCommand.Run(); err != nil {
-		panic(err)
+		log.Printf("gamescope finished with error: %v", err)
 	}
 }
 
